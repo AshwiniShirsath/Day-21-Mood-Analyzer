0f7bd951ced0079d2f666adeab773a9940ab177f
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-05-11 17:49:36 +0530

    [ADDED] : Added I am in Happy mood should return sad

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyserMain.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyserMain.java
@@ -5,14 +5,18 @@ package com.bridgelabz.moodanalyzer;
  */
 
 public class MoodAnalyserMain {
+    private String message;
     /**
-     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     * create a default constructor name as MoodAnalyserMain
      */
     public MoodAnalyserMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
+    public MoodAnalyserMain(String message) {
+        this.message=message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
